dd0ba8275b98fbdfccd16f3384e19c48e38d41e1
Assaf <assafyechiel@gmail.com>
2023-11-07 18:27:29 +0200

    Add comments to script

--- script.js
+++ script.js
@@ -12,22 +12,15 @@ const equalsButton = document.querySelector('.equals');
 
 const floatButton = document.querySelector('.float');
 
-// let input = prompt("Input: ");
-// let inputArray = input.split(' ');
-// let firstNumber = +inputArray[0];
-// let operator = inputArray[1];
-// let secondNumber = +inputArray[2];
-// let result = 0;
-
+// Can I get rid of these?
 let inputValue = 0;
 display.textContent = inputValue;
-let secondValue;
-// let operatorValue;
 
 let valueStorage = {
     initialValue: null,
 };
 
+// Assigns to specified object member and displays the selected number input 
 numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click', () => {
         if (valueStorage["initialValue"] != null && valueStorage["operatorValue"] == undefined) {
@@ -50,6 +43,8 @@ numberButtons.forEach((numberButton) => {
     })
 })
 
+// Assigns selected operator input to object member
+// Calculates and displays result if operator has been input after first number pair (e.g. 4+5+ will display 9)
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener('click', () => {
         if (valueStorage["operatorValue"] !== undefined) {
@@ -66,6 +61,7 @@ operatorButtons.forEach((operatorButton) => {
     })
 })
 
+// Calculates and displays the result
 equalsButton.addEventListener('click', () => {
     operate(valueStorage["operatorValue"], valueStorage["initialValue"], valueStorage["nextValue"]);
     display.textContent = valueStorage["initialValue"];
@@ -73,20 +69,16 @@ equalsButton.addEventListener('click', () => {
     console.log(valueStorage["initialValue"]);
 })
 
+// Arithmetic functions
 const add = () => valueStorage["initialValue"] = +valueStorage["initialValue"] + +valueStorage["nextValue"];
-
 const subtract = () => valueStorage["initialValue"] = +valueStorage["initialValue"] - +valueStorage["nextValue"];
-
 const multiply = () => valueStorage["initialValue"] = +valueStorage["initialValue"] * +valueStorage["nextValue"];
-
 const divide =  () => valueStorage["initialValue"] = +valueStorage["initialValue"] / +valueStorage["nextValue"];
 
+// Calls on selected operator arithmetic function
 function operate() {
     return valueStorage["operatorValue"] === '+' ? add()
     : valueStorage["operatorValue"] === '-' ? subtract()
     : valueStorage["operatorValue"] === '*' ? multiply()
     : valueStorage["operatorValue"] === '/' ? divide() : false
 }
-
-// operate(operator, firstNumber, secondNumber);
-// console.log(result);

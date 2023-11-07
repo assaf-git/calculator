479c90e3aff845b007caf602df3ded4b8c51927f
Assaf <assafyechiel@gmail.com>
2023-11-07 19:10:57 +0200

    Create result value object member

--- script.js
+++ script.js
@@ -23,19 +23,19 @@ let valueStorage = {
 // Assigns to specified object member and displays the selected number input 
 numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click', () => {
-        if (valueStorage["initialValue"] != null && valueStorage["operatorValue"] == undefined) {
+        if (valueStorage["initialValue"] != null && valueStorage["operatorValue"] == undefined) { // Allows more than one digit for initialValue and assigns it
             valueStorage["initialValue"] += numberButton.id;
             display.textContent = valueStorage["initialValue"];
             console.log(valueStorage["initialValue"]);
-        } else if (valueStorage["initialValue"] != null && valueStorage["nextValue"] != null) {
+        } else if (valueStorage["initialValue"] != null && valueStorage["nextValue"] != null) { // Allows more than one digit for nextValue and assigns it
             valueStorage["nextValue"] += numberButton.id;
             display.textContent = valueStorage["nextValue"];
             console.log(valueStorage["nextValue"]);
-        } else if (valueStorage["initialValue"] != null) {
+        } else if (valueStorage["initialValue"] != null) {      // Assigns nextValue
             valueStorage["nextValue"] = numberButton.id;
             display.textContent = valueStorage["nextValue"];
             console.log(valueStorage["nextValue"]);
-        } else {
+        } else {                                                // Assigns initialValue
             valueStorage["initialValue"] = numberButton.id;
             display.textContent = valueStorage["initialValue"];
             console.log(valueStorage["initialValue"]);
@@ -44,12 +44,12 @@ numberButtons.forEach((numberButton) => {
 })
 
 // Assigns selected operator input to object member
-// Calculates and displays result if operator has been input after first number pair (e.g. 4+5+ will display 9)
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener('click', () => {
-        if (valueStorage["operatorValue"] !== undefined) {
+        if (valueStorage["operatorValue"] !== undefined) { // Calculates and displays result if operator has been input after first number pair (e.g. 4+5+ will display 9)
             operate(valueStorage["operatorValue"], valueStorage["initialValue"], valueStorage["nextValue"]);
-            display.textContent = valueStorage["initialValue"];
+            valueStorage["initialValue"] = valueStorage["resultValue"];
+            display.textContent = valueStorage["resultValue"];
             valueStorage["operatorValue"] = operatorButton.id;
             valueStorage["nextValue"] = null;
             console.log(valueStorage["initialValue"]);
@@ -64,16 +64,17 @@ operatorButtons.forEach((operatorButton) => {
 // Calculates and displays the result
 equalsButton.addEventListener('click', () => {
     operate(valueStorage["operatorValue"], valueStorage["initialValue"], valueStorage["nextValue"]);
-    display.textContent = valueStorage["initialValue"];
+    valueStorage["initialValue"] = valueStorage["resultValue"];
+    display.textContent = valueStorage["resultValue"];
     valueStorage["operatorValue"] = null;
     console.log(valueStorage["initialValue"]);
 })
 
 // Arithmetic functions
-const add = () => valueStorage["initialValue"] = +valueStorage["initialValue"] + +valueStorage["nextValue"];
-const subtract = () => valueStorage["initialValue"] = +valueStorage["initialValue"] - +valueStorage["nextValue"];
-const multiply = () => valueStorage["initialValue"] = +valueStorage["initialValue"] * +valueStorage["nextValue"];
-const divide =  () => valueStorage["initialValue"] = +valueStorage["initialValue"] / +valueStorage["nextValue"];
+const add = () => valueStorage["resultValue"] = +valueStorage["initialValue"] + +valueStorage["nextValue"];
+const subtract = () => valueStorage["resultValue"] = +valueStorage["initialValue"] - +valueStorage["nextValue"];
+const multiply = () => valueStorage["resultValue"] = +valueStorage["initialValue"] * +valueStorage["nextValue"];
+const divide =  () => valueStorage["resultValue"] = +valueStorage["initialValue"] / +valueStorage["nextValue"];
 
 // Calls on selected operator arithmetic function
 function operate() {
